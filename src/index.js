eb72f7616500d0728167cc8616063cd23bba17b3
ottovbismarck <fritzj96@gmail.com>
2018-07-01 02:07:03 -0700

    added imported SearchBar to the app component

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ const API_KEY = 'AIzaSyBk6dKj2i4RFlKk2mNtPZyzd-7m-8fImmA';
 
 // Create a new Component. It should produce some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
 //Put the component's generated HTML into the DOM
