cfed8bbdc75eec1b787401ad1cf27ef9854ba4da
ottovbismarck <fritzj96@gmail.com>
2018-07-01 02:56:54 -0700

    Used ES6 syntax to condense key-value pair on state setting syntax.

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = { videos: [] };
-        YTSearch({ key: API_KEY, term: 'Surfboards' } , data => {
-            this.setState({ videos: data });
+        YTSearch({ key: API_KEY, term: 'Surfboards' } , videos => {
+            this.setState({ videos });
         });
     }
     render() {
