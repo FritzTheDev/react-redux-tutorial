5bab9fea92f2c51c00012cc9d5e50896068322a4
ottovbismarck <fritzj96@gmail.com>
2018-07-01 02:47:48 -0700

    Testing functionality of Youtube Search API Sec 2 lec 22

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // Get Access to React using CommonModules
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBk6dKj2i4RFlKk2mNtPZyzd-7m-8fImmA';
 
+YTSearch({key: API_KEY, term: 'Surfboards'}, function(data) {
+    console.log(data);
+});
 // Create a new Component. It should produce some HTML
 const App = () => {
     return (
