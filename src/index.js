6c55fa22dd38351da613fae33e251b5e3fad97dd
ottovbismarck <fritzj96@gmail.com>
2018-07-01 01:59:40 -0700

    Added Base Components & added youtube-api-search library

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyBk6dKj2i4RFlKk2mNtPZyzd-7m-8fImmA';
+
 // Create a new Component. It should produce some HTML
 const App = () => {
     return <div>Hi!</div>;
