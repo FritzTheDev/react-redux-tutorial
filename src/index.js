a67f1441616bff491c24a3a2fe951fe3f3e9b84d
ottovbismarck <fritzj96@gmail.com>
2018-07-01 01:36:28 -0700

    sec1 lec8 Imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+// Get Access to React using CommonModules
+import React from 'react';
+
 // Create a new Component. It should produce some HTML
-const app = () => {
+const App = () => {
     return <div>Hi!</div>;
 }
 
 //Put the component's generated HTML into the DOM
+React.render(App);
