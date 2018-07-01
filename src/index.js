89747468da6a88f627c13ca1eb0998245da08ec3
ottovbismarck <fritzj96@gmail.com>
2018-07-01 02:54:43 -0700

    Added Youtube Search to constructor to get data on instantiation.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Get Access to React using CommonModules
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -7,17 +7,21 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBk6dKj2i4RFlKk2mNtPZyzd-7m-8fImmA';
 
-YTSearch({key: API_KEY, term: 'Surfboards'}, function(data) {
-    console.log(data);
-});
-// Create a new Component. It should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+        YTSearch({ key: API_KEY, term: 'Surfboards' } , data => {
+            this.setState({ videos: data });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
-//Put the component's generated HTML into the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
